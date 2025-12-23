746f758f79d7cd7e8c3a33408b131cfe92f236b8
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-23 17:53:20 +0530

    Update bashscript.sh

--- scripts/bashscript.sh
+++ scripts/bashscript.sh
@@ -90,7 +90,8 @@ node scripts/build_category_avg.js
 # Commit & push data
 # -------------------------------
 echo "🚀 Pushing updated data to GitHub..."
-
+git config user.name "amfi-bot"
+git config user.email "bot@github.com"
 git add .
 git commit -m "Daily MF & Nifty data update" || echo "ℹ️ No changes to commit"
 git push
